c1e8792d6eb91a3f68a9e7b893bdfdcbb093443a
Raphael Betz <raphaelbetz@gmail.com>
2018-06-14 14:09:36 +0200

    usage in add_songs.tpl to dynamically show albums of selected artist

---
+++ public_html/js/add_song.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var uploadFormArtist = document.getElementById("artist");
+    var uploadFormAlbum = document.getElementById("album");
+
+    uploadFormArtist.addEventListener('change', function() {
+        while (uploadFormAlbum.options.length > 0) {
+            uploadFormAlbum.options[0].remove();
+        }
+        uploadFormAlbum.options.add(createOptionObject("none", "Keinem Album zugeordnet"));
+
+
+        var selectedArtistId = uploadFormArtist.options[uploadFormArtist.selectedIndex].value;
+
+        fetch('http://localhost/musicstreaming/public_html/?p=api_albums&artistid=' + selectedArtistId).then(function(rawResponse) {
+            rawResponse.json().then(function(json){
+                if (json != null){
+                    for(var i = 0; i < json.length; i++) {
+                        uploadFormAlbum.options.add(createOptionObject(json[i]["id"],json[i]["name"]));
+                    }
+                }
+            })
+        })
+        uploadFormAlbum.disabled = false;
+    });
+
+
+    function createOptionObject(value, text) {
+        var option = document.createElement("option");
+        option.value = value;
+        option.text = text;
+        return option;
+    }
+});
